d0623cd6bc05657d1a5e197d7f7caf6e09949c68
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2021-03-18 23:01:24 +0700

    String

--- string_format.py
+++ string_format.py
@@ -4,6 +4,8 @@ nama_tengah = "Mahmud"
 nama_belakang = "Abubakar"
 
 # versi classic
-nama_lengkap = "Assalamualaikum " + nama_depan +" "+ nama_tengah + " " + nama_belakang
+# nama_lengkap = "Assalamualaikum " + nama_depan +" "+ nama_tengah + " " + nama_belakang
+# versi simple, untuk mengenali variable String di python jangan lupa gunakan "f" didepannya
+nama_lengkap = f"Assalamualaikum {nama_depan} {nama_tengah} {nama_belakang}"
 
 print(nama_lengkap)
