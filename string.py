1fa49bb86404d1043c34968a3824a07fbbef9bcb
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2021-03-18 22:56:39 +0700

    string

---
+++ string.py
@@ -0,0 +1,10 @@
+# Belajar String
+
+nama = "Mustofa Mahmud"
+kota = 'Jakarta'
+
+
+nama_depan = "Mustofa"
+nama_belakang = "Mahmud"
+nama_lengkap = nama_depan + nama_belakang
+print(nama_lengkap)
---
+++ string_format.py
@@ -0,0 +1,9 @@
+# Belajar String Format
+nama_depan = "Mustofa"
+nama_tengah = "Mahmud"
+nama_belakang = "Abubakar"
+
+# versi classic
+nama_lengkap = "Assalamualaikum " + nama_depan +" "+ nama_tengah + " " + nama_belakang
+
+print(nama_lengkap)
